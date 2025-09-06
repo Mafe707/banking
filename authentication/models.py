37ca7cbcf7e0b4ec345ab3170e3ea64df96c03c9
Mafe707 <fernanditarevelo@gmail.com>
2025-09-06 02:58:08 -0500

    The four models were created, and four data entries were added to each for the workshop

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class Countries(models.Model):
+    name = models.CharField(max_length=50)
+    abrev = models.CharField(max_length=5)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+class Departments(models.Model):
+    name = models.CharField(max_length=50)
+    abrev = models.CharField(max_length=5)
+    id_countries = models.ForeignKey(Countries, on_delete=models.CASCADE, related_name='departments')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+class Cities(models.Model):
+    name = models.CharField(max_length=50)
+    abrev = models.CharField(max_length=5)
+    id_departments = models.ForeignKey(Departments, on_delete=models.CASCADE, related_name='cities')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+class Users(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    mobile_phone = models.CharField(max_length=15, blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    status = models.BooleanField(default=True)
+    id_cities = models.ForeignKey(Cities, on_delete=models.CASCADE, related_name='users')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
 class User(models.Model):
     firstname = models.CharField(max_length=20)
     lastname = models.CharField(max_length=20, blank=True)
