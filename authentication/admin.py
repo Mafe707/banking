102204c5e60a29710152f6ba0cfbbd85995182af
Mafe707 <fernanditarevelo@gmail.com>
2025-09-26 12:51:40 -0500

    Models in admin.py

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from.models import Countries,Departments, Cities, Users
 # Register your models here.
+admin.site.register(Countries)
+admin.site.register(Departments)
+admin.site.register(Cities)
+admin.site.register(Users)
